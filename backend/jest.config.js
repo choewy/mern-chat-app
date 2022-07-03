e16bb8a843ce74bbd1bd58d12570445e10f5d866
choewy <choewy32@gmail.com>
2022-07-03 20:03:27 +0900

    [init] node.js express initialized

---
+++ backend/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  roots: ['<rootDir>/__test__'],
+  testMatch: [
+    '**/__test__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
