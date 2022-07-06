b8659c0697403c2dab7a834c9b1ee4694416835f
choewy <choewy32@gmail.com>
2022-07-06 10:02:49 +0900

    [feat] application 실행

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,75 +1,10 @@
-import dotenv from 'dotenv';
-import express, { Application } from 'express';
-import cors from 'cors';
-import routes from './routes';
-import mongoose from 'mongoose';
+import express from 'express';
+import settings from './setting';
 
-const initConfigs = {
-  appConfig: {
-    port: 0,
-  },
-  mongoConfig: {
-    host: '127.0.0.1',
-    port: 27017,
-    user: '',
-    password: '',
-    database: 'admin',
-  },
-};
-
-export class App {
-  private app: Application;
-  private configs = initConfigs;
-
-  constructor() {
-    this.app = express();
-    this.readEnvironment();
-    this.setGlobalMiddlewares();
-    this.setRouters();
-  }
-
-  private readEnvironment() {
-    ['.env.local'].forEach((path) => {
-      dotenv.config({ path });
-    });
-
-    this.configs = {
-      appConfig: {
-        port: Number(process.env.PORT),
-      },
-      mongoConfig: {
-        host: String(process.env.MONGO_HOST),
-        port: Number(process.env.MONGO_PORT),
-        user: String(process.env.MONGO_USER),
-        password: String(process.env.MONGO_PASSWORD),
-        database: String(process.env.MONGO_DATABASE),
-      },
-    };
-  }
-
-  private setGlobalMiddlewares() {
-    this.app.use(express.urlencoded({ extended: true }));
-    this.app.use(express.json());
-    this.app.use(cors({ origin: '*', credentials: true }));
-  }
-
-  private setRouters() {
-    routes.forEach((route) => {
-      const { path, router } = route;
-      this.app.use(path, router);
-    });
-  }
-
-  listen() {
-    const { appConfig, mongoConfig } = this.configs;
-    const connection = `mongodb://${mongoConfig.user}:${mongoConfig.password}@${mongoConfig.host}:${mongoConfig.port}/${mongoConfig.database}`;
-    mongoose
-      .connect(connection)
-      .then(() => {
-        this.app.listen(appConfig.port, () =>
-          console.log(`Server running on port ${appConfig.port}`)
-        );
-      })
-      .catch((error) => console.log(error));
+declare global {
+  interface MongoResult {
+    _doc: any;
   }
 }
+
+export default settings(express()).forEach((setting) => setting());
